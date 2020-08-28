82418d319b67c553c6d76b9ac9a6b5c50b343b1d
davi <davi.p.m94@gmail.com>
2020-08-28 20:19:41 -0300

    update routes

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
+import TeacherForm from "./pages/TeacherForm";
+import TeacherList from "./pages/TeacherList";
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Landing} />
+      <Route path="/study" component={TeacherForm} />
+      <Route path="/give-classes" component={TeacherList} />
     </Switch>
   );
 }
