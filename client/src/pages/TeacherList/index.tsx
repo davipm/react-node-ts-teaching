21f18aa9fda14721ee0831602b5b379ea2340df4
davi <davi.p.m94@gmail.com>
2020-09-01 20:07:49 -0300

    adding teacher list

---
+++ client/src/pages/TeacherList/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+function TeacherList() {
+  return (
+    <Container>
+      <p>Teacher List</p>
+    </Container>
+  );
+}
+
+export default TeacherList;
