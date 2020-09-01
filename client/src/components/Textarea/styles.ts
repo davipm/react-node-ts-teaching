a1a21f807858c992998c1c9de6c95a53e26da331
davi <davi.p.m94@gmail.com>
2020-09-01 00:09:30 -0300

    fixing position

--- client/src/components/Textarea/styles.ts
+++ client/src/components/Textarea/styles.ts
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 export const Container = styled.div`
-  position: revert;
+  position: relative;
 
   & + & {
     margin-top: 1.4rem;
